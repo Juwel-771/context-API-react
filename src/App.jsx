76e2ed41f9ec6f27af646aa0e43bb6442ea12835
Juwel-771 <juwelah99@gmail.com>
2025-03-15 06:45:57 +0600

    card and them UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './component/Login'
 import Profile from './component/Profile'
+import { ThemeProvider } from './context/theme'
 
 function App() {
-const [count, setCount] = useState(0)
+
+  const [themeMode, setThemMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemMode("dark");
+  };
+
+  // actual theme change
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode);
+  },[themeMode]);
 
 return (
 // <UserContextProvider>
@@ -14,7 +30,8 @@ return (
   // <Profile></Profile>
   // </UserContextProvider>
 
-    <div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
           {/* theme button */}
@@ -25,6 +42,7 @@ return (
         </div>
       </div>
     </div>
+    </ThemeProvider>
 
   )
 }
